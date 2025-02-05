53119ce581cc75124a0040291b4ec422cdb26f2e
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-02-04 20:44:10 -0600

    Add outer and inner loop for bubble sort.

---
+++ bubble-sort.rb
@@ -0,0 +1,10 @@
+def bubble_sort(array)
+    (array.length - 1).times do |pass|
+      (0...array.length - 1 - pass).each do |index|
+        
+      end
+    end
+  end
+
+p bubble_sort([4,3,78,2,0,2])
+# => [0,2,2,3,4,78]
