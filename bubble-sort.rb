858f17cc3d4facbdd0515a33f21cded0da55de94
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-02-05 08:33:05 -0600

    Complete inner logic and add logic for swapping.
    Completed logic for if array[index] > array[index + 1].
    Also added flag for if the pass had no swaps. Allows
    for better performance.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,10 +1,16 @@
 def bubble_sort(array)
-    (array.length - 1).times do |pass|
-      (0...array.length - 1 - pass).each do |index|
-        
+  (array.length - 1).times do |pass|
+    swapped = false
+    (0...array.length - 1 - pass).each do |index|
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+        swapped = true
       end
     end
+    break unless swapped
   end
+  array
+end
 
-p bubble_sort([4,3,78,2,0,2])
-# => [0,2,2,3,4,78]
+p bubble_sort([4, 3, 78, 2, 0, 2])
+# => [0, 2, 2, 3, 4, 78]
